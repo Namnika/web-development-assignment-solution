e8ba73254fb3a388e5398fec5e6ee11829d4d343
Namnika <janbandhunamnika@gmail.com>
2024-03-17 23:47:44 +0530

    Update centralization styles

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,15 +4,17 @@ import LoginLogo2 from "./images/Group 3@2x.png";
 export default function LoginPage() {
   return (
     <div className="login-wrapper">
-      <img src={LoginLogo} alt="Login Logo" srcSet={LoginLogo2} />
-    <div className="login-input">
-    <h6>#We are Electric</h6>
-      <input type="email" placeholder="E-Mail" className="email"/>
-      <input type="password" placeholder="Password" className="password"/>
-      <button>Login</button>
+      <div className="login-input">
+        <img src={LoginLogo} alt="Login Logo" srcSet={LoginLogo2} />
+        <h6>#We are Electric</h6>
+        <input type="email" placeholder="E-Mail" className="email" />
+        <input type="password" placeholder="Password" className="password" />
+        <button>Login</button>
+
+        <a href="#link" className="forgot-link">
+          <p>Forgot password?</p>
+        </a>
       </div>
-      <p>Forgot password?</p>
-     
     </div>
   );
 }
