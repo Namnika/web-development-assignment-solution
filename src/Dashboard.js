43aef5f7d01c8e8184677f1be6a3afc8ab6aa49b
Namnika <janbandhunamnika@gmail.com>
2024-03-18 14:52:40 +0530

    add dashboard layout

---
+++ src/Dashboard.js
@@ -0,0 +1,36 @@
+export default function Dashboard({ productivity }) {
+  return (
+    <section>
+      <label>Employee Productivity Dashboard</label>
+      <div className="productivity">
+        {productivity.map((prod) => {
+          return (
+            <>
+              <div className="prodlevel">
+                <h4>{prod.prodname}</h4>
+                <div
+                  class="progress"
+                  role="progressbar"
+                  aria-label="Basic example"
+                  aria-valuenow="25"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                >
+                  <div
+                    class="progress-bar"
+                    style={{
+                      width: `${prod.prodlevel}`,
+                      backgroundColor: "#36A546",
+                    }}
+                  ></div>
+                </div>
+              </div>
+
+              <span>{prod.prodlevel}</span>
+            </>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,3 +1,4 @@
+import Dashboard from "./Dashboard";
 import UserProfile from "./images/Group 46.png";
 import UserProfile2 from "./images/Group 46@2x.png";
 import MoptroLogo from "./images/moptro logo.png";
@@ -24,38 +25,7 @@ export default function HomePage() {
         alt="Moptro Logo"
         srcSet={MoptroLogo2}
       />
-      <section>
-        <label>Employee Productivity Dashboard</label>
-        <div className="productivity">
-          {productivity.map((prod) => {
-            return (
-              <>
-                <div className="prodlevel">
-                  <h4>{prod.prodname}</h4>
-                  <div
-                    class="progress"
-                    role="progressbar"
-                    aria-label="Basic example"
-                    aria-valuenow="25"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >
-                    <div
-                      class="progress-bar"
-                      style={{
-                        width: `${prod.prodlevel}`,
-                        backgroundColor: "#36A546",
-                      }}
-                    ></div>
-                  </div>
-                </div>
-
-                <span>{prod.prodlevel}</span>
-              </>
-            );
-          })}
-        </div>
-      </section>
+      <Dashboard productivity={productivity} />
     </div>
   );
 }
