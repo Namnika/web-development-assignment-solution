34a5e9b84bed3ce0e4889a69be88da02a7a9774c
Namnika <janbandhunamnika@gmail.com>
2024-03-18 13:32:19 +0530

    add productivity styles

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,13 @@
 import UserProfile from "./images/Group 46.png";
 import UserProfile2 from "./images/Group 46@2x.png";
+import MoptroLogo from "./images/moptro logo.png";
+import MoptroLogo2 from "./images/moptro logo@2x.png";
+import { productivity } from "./utils/Productivity";
+
 export default function HomePage() {
   return (
-    <div>
-      <nav className="navbar">
+    <div className="dashboard">
+      <nav>
         <img
           src={UserProfile}
           width={40}
@@ -12,6 +16,45 @@ export default function HomePage() {
           srcSet={UserProfile2}
         />
       </nav>
+
+      <img
+        className="hompage-logo"
+        src={MoptroLogo}
+        alt="Moptro Logo"
+        srcSet={MoptroLogo2}
+      />
+      <section>
+        <label>Employee Productivity Dashboard</label>
+        <div className="productivity">
+          {productivity.map((prod) => {
+            return (
+              <>
+                <div className="prodlevel">
+                  <h4>{prod.prodname}</h4>
+                  <div
+                    class="progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      class="progress-bar"
+                      style={{
+                        width: `${prod.prodlevel}`,
+                        backgroundColor: "#36A546",
+                      }}
+                    ></div>
+                  </div>
+                </div>
+
+                <span>{prod.prodlevel}</span>
+              </>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/utils/Productivity.js
@@ -0,0 +1,32 @@
+export const productivity = [
+    {
+        id: 1,
+        prodname: 'Productivity on Monday',
+        prodlevel: '4%'
+    },
+    {
+        id: 2,
+        prodname: 'Productivity on Tuesday',
+        prodlevel: '92%'
+    },
+    {
+        id: 3,
+        prodname: 'Productivity on Wednesday',
+        prodlevel: '122%',
+    },
+    {
+        id: 4,
+        prodname: 'Productivity on Thursday',
+        prodlevel: '93%',
+    },
+    {
+        id: 5,
+        prodname: 'Productivity on Friday',
+        prodlevel: '89%',
+    },
+    {
+        id: 6,
+        prodname: 'Productivity on Saturday',
+        prodlevel: '98%',
+    }
+]
